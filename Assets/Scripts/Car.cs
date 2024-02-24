425dce0310c891dca250e211416e0d1154af1570
Tony H.Vu <tony.v95@hotmail.com>
2024-02-24 23:54:59 +0100

    Added Movement + Steer Script, Steer with Canvas Touch, + Cinemachine

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] float speedGainPerSecond = 1.2f;
+    [SerializeField] float turnSpeed;
+
+    public int steerValue; //-1 = left, 0 = forward, 1 = right
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(speed < 50f)
+        {
+            speed += speedGainPerSecond * Time.deltaTime;
+
+        }
+        else
+        {
+            speed = 50f;
+        }
+
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+ 
+    public void Steer(int value)
+    {
+        steerValue = value;
+    }
+}
